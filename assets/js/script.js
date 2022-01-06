d90c0e7cc96082ddbb8690a6c5ef503f8b616f50
Michael Klein <Michaelklein1997@gmail.com>
2022-01-06 17:57:26 -0600

    stuck on getting each input to save as its own variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,54 +4,32 @@ function getDate() {
     console.log("Today's date is " +  currentDate);
     $('#currentDay').text(currentDate)
 }
-
 setInterval(getDate,1)
 
 //change color of timeblocks based on what time it is
-
-// var eightAMEl = $('#8am');
-// var nineAMEl = $('#9am');
-// var tenAMEl = $('#10am');
-// var elevenAMEl = $('#11am');
-// var twelvePMEl = $('#12pm');
-// var onePMEl = $('#1pm');
-// var twoPMEl = $('#2pm');
-// var threePMEl = $('#3pm');
-// var fourPMEl = $('#4pm');
-// var fivePMEl = $('#5pm');
-
-
-var timeblockArray = [$('#8am'),$('#9am'),$('#10am'),$('#11am'),$('#12pm'),$('#1pm'),$('#2pm'),$('#3pm'),$('#4pm'),$('#5pm')] 
-
-timeblockColor()
 function timeblockColor() {
     var timeArray = [8,9,10,11,12,13,14,15,16,17]
+    var timeBlockElArray = [$('#8am'),$('#9am'),$('#10am'),$('#11am'),$('#12pm'),$('#1pm'),$('#2pm'),$('#3pm'),$('#4pm'),$('#5pm')] 
     var currentTime = Number(moment().format('HH'));
     console.log("Current Hour is " + currentTime);
 
     for(var i = 0; i < timeArray.length; i++) {
-        if (currentTime < timeArray[i]) {
-            timeblockArray[i].addClass('future')
-        } else if (currentTime == timeArray[i]) {
-            timeblockArray[i].addClass('present')
-        } else if (currentTime > timeArray[i]) {
-            timeblockArray[i].addClass('past')
+        if (currentTime < timeArray[i]) {               //if "currentTime" is "scheduledTime"   
+            timeBlockElArray[i].addClass('future')        //then change class to "future"
+        } else if (currentTime == timeArray[i]) {       //if "currentTime" is == "scheduledTime"
+            timeBlockElArray[i].addClass('present')       //then change class to "present"
+        } else if (currentTime > timeArray[i]) {        //if "currentTime" is > "scheduledTime"
+            timeBlockElArray[i].addClass('past')          //then chnage class to "past"
         }
     }
-
-
-    //if "currentTime" is < "scheduledTime"
-    //then change class to "future"
-    //if "currentTime" is == "scheduledTime"
-    //then change class to "present"
-    //if "currentTime" is > "scheduledTime"
-    //then chnage class to "past"
 }
+setInterval(timeblockColor(),1000)  //will update color when hour changes without having to refresh
 
 //save button
-
 $('.container').on('click','.saveBtn', function(event) {
     console.log("click");
     console.log(typeof(event.target))
-    console.log($(event.target).siblings("input"));
+    localStorage.setItem("event", $(event.target).siblings("input").val());
 })
+var savedBlanks = ["","","","","","","","","",""]
+localStorage.getItem("event")
