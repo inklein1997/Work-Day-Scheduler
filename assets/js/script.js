1c2730a37a5cd858801794b35e34bb1df057efb7
Michael Klein <Michaelklein1997@gmail.com>
2022-01-09 17:44:30 -0600

    fixed bug that prevented input boxes from changing colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ function timeblockColor() {
     var timeArray = [8,9,10,11,12,13,14,15,16,17]
     var timeBlockElArray = [$('#8am'),$('#9am'),$('#10am'),$('#11am'),$('#12pm'),$('#1pm'),$('#2pm'),$('#3pm'),$('#4pm'),$('#5pm')] 
     var currentTime = Number(moment().format('HH'));
+    console.log("Test")
 
     for(var i = 0; i < timeArray.length; i++) {
         if (currentTime < timeArray[i]) {               //if "currentTime" is "scheduledTime"   
@@ -22,7 +23,7 @@ function timeblockColor() {
         }
     }
 }
-setInterval(timeblockColor, 1000)  //will update color when hour changes without having to refresh
+setInterval(timeblockColor(), 1000)  //will update color when hour changes without having to refresh
 
 // var savedEvents = ["","","","","","","","","",""]
 
@@ -31,7 +32,7 @@ $('.container').on('click','.save', function(event) {   //uses event delegation 
     console.log($(event.target))
     var index = $(event.target).parents().siblings("input").attr("data-index")      //used to recieve position of event 
     var userInput = $(event.target).parents().siblings("input").val()       //stores what user typed into variable
-    console.log("The user inputted: " userInput)
+    console.log("The user inputted: " + userInput)
     savedEvents.splice(index, 1, userInput)     //creates new array with values meant to be saved
     console.log(savedEvents)
     localStorage.setItem("savedEvents", JSON.stringify(savedEvents))    //saves array as string in localstorage
